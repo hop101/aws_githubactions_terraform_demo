8acbd3bed9ad1d354289e3321a79d3f4e79e9d35
hop101 <70587704+hop101@users.noreply.github.com>
2021-02-01 00:59:13 +0530

    Update bootstrap_variables.tf

--- modules/bootstrap/bootstrap_variables.tf
+++ modules/bootstrap/bootstrap_variables.tf
@@ -2,9 +2,27 @@
 # Define variables for Azure DevOps Seed Module
 ##
 
-variable "name_of_s3_bucket" {}
-variable "dynamo_db_table_name" {}
-variable "iam_user_name" {}
-variable "ado_iam_role_name" {}
-variable "aws_iam_policy_permits_name" {}
-variable "aws_iam_policy_assume_name" {}
+variable "name_of_s3_bucket" { 
+  default = "hop101_confideo_terraform_state"
+  type    = "string" 
+  }
+variable "dynamo_db_table_name" {
+  default = "terraform_state_locking"
+  type    = "string"
+  }
+variable "iam_user_name" {
+  default = "terraform_github_actions"
+  type    = "string"
+  }
+variable "ado_iam_role_name" {
+  default = "terraform_role"
+  type    = "string"
+  }
+variable "aws_iam_policy_permits_name" {
+  default = "terraform_role_name"
+  type    = "string"
+  }
+variable "aws_iam_policy_assume_name" {
+  default = "terraform_policy"
+  type    = "string"
+  }
